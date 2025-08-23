7f2adf7482ad9e674c1c0f00fd26f68ba19d1d4e
Gregory Harris <63122765+gregoryharris869@users.noreply.github.com>
2025-08-23 15:20:57 -0500

    started

---
+++ Travel_Web/main.js
@@ -0,0 +1,26 @@
+const menuBtn = document.getElementById("menu-btn");
+const navLinks = document.getElementById("nav-links");
+const menuBtnIcon = menuBtn.querySelector("i");
+
+menuBtn.addEventListener("click", (e) => {
+  const isOpen = navLinks.classList.contains("open");
+  menuBtnIcon.setAttribute("class", isOpen ? "ri-menu-line" : "ri-close-line");
+  if (isOpen) {
+    navLinks.classList.add("close");
+    navLinks.addEventListener(
+      "animationend",
+      (e) => {
+        navLinks.classList.remove("open");
+        navLinks.classList.remove("close");
+      },
+      { once: true }
+    );
+  } else {
+    navLinks.classList.add("open");
+  }
+});
+
+navLinks.addEventListener("click", (e) => {
+  navLinks.classList.remove("open");
+  menuBtnIcon.setAttribute("class", "ri-menu-line");
+});
